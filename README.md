ca9af277ff43eafe248a4afa1d83a9c9e445a020
mwb86 <masonwbrown@gmail.com>
2016-12-05 10:21:12 -0500

    added moves counter and README

---
+++ README.md
@@ -0,0 +1,36 @@
+"The Tower of Hanoi (also called the Tower of Brahma or Lucas' Tower, and sometimes pluralized) is a mathematical game or puzzle. It consists of three rods, and a number of disks of different sizes which can slide onto any rod.....The puzzle was invented by the French mathematician Édouard Lucas in 1883. There is a story about an Indian temple in Kashi Vishwanath which contains a large room with three time-worn posts in it surrounded by 64 golden disks. Brahmin priests, acting out the command of an ancient prophecy, have been moving these disks, in accordance with the immutable rules of the Brahma, since that time. The puzzle is therefore also known as the Tower of Brahma puzzle. According to the legend, when the last move of the puzzle will be completed, the world will end. It is not clear whether Lucas invented this legend or was inspired by it."
+https://en.wikipedia.org/wiki/Tower_of_Hanoi
+
+
+
+Play my verion of the game here https://mwb86.github.io/towers-of-hanoi/#
+
+
+To play this game you go to the New Game menu and select the number of discs. 4 discs is the easiest setting. Additional discs will make the game exponentially longer. You click on the first tower of discs and it removes the top one. You then place this piece by clicking on one of the other towers. It is against the rules to place this piece on top of a piece that is smaller. Win the game by moving the whole set of discs to one of the other towers.
+
+-Building the game-
+
+The first thing I did was think about the logic of the game. It breaks down to 3 arrays and the interaction between them.
+
+1)The player is only allowed to pick(.shift()) one value at a time (var pieceInMotion).
+
+2)The value will be placed or removed from the beginning of the clicked array(the top of the tower selected).
+
+3)The value of pieceInMotion must be less than the first value in the array(tower) selected for placing the disc.
+
+4)The game stops amd declares a win when the original tower1(var winningtower is tower1 before any disc moves) is equal to either of the other arrays (tower1, tower2)
+
+5)The player is presented these arrays in the form of towers with the wood disc images. This allows them to visually interpret the arrays through volume instead of a number. These towers are interactive and allow the user to affect the arrays with clicks.
+
+My game consists of HTML, CSS, animate.css(https://daneden.github.io/animate.css/), Javascript, and Jquery.
+The interactivity is created through click events that activate for loops and if/else statements. These are controlling the data in the arrays and printing the data to the browser in the form of wood discs on towers.
+
+-Unsolved problems-
+
+1)My page is not designed for mobile yet. Some of the things move to the wrong place when the screen shrinks.
+
+2)I want to condense the code more.
+
+3)I'd like to add a timer to go with the moves counter.
+
+4)The last thing I want to add is an ability to close the game and come back to it later with the pieces in the same place.
--- script.js
+++ script.js
@@ -4,23 +4,83 @@ var tower2 = [];
 var tower3 = [];
 var pieceinmotion = undefined;
 var audio = $("#mysoundclip")[0];
-
+var moves = 0;
+/*
+drawTowers = switch(towerID){
+    case tower1:
+    for (var each in tower1) {
+        $('.tower1').append("<div class ='animated shake' id=gamepiece" + tower1[each] + "></div>");
+        $('#score').text(moves);
+    }
+        break;
+    case tower2:
+    for (var each in tower1) {
+        $('.tower2').append("<div class ='animated shake' id=gamepiece" + tower2[each] + "></div>");
+        $('#score').text(moves);
+    }
+        break;
+    case tower3:
+    for (var each in tower1) {
+        $('.tower3').append("<div class ='animated shake' id=gamepiece" + tower3[each] + "></div>");
+        $('#score').text(moves);
+    }
+        break;
+    default:
+}}}
+*/
 drawTower1 = function() {
     for (var each in tower1) {
         $('.tower1').append("<div class ='animated shake' id=gamepiece" + tower1[each] + "></div>");
+        $('#score').text(moves);
     }
 };
 drawTower2 = function() {
     for (var each in tower2) {
         $('.tower2').append("<div class ='animated shake' id=gamepiece" + tower2[each] + "></div>");
+        $('#score').text(moves);
     }
 };
 drawTower3 = function() {
     for (var each in tower3) {
         $('.tower3').append("<div class ='animated shake' id=gamepiece" + tower3[each] + "></div>");
+        $('#score').text(moves);
     }
 };
-
+/*
+interactwithtower = function(towerID){
+  console.log(towerID);
+  if (pieceinmotion === undefined) {
+      pieceinmotion = towerID.shift();
+      $(this).children().remove();
+  } else if (towerID[0] === undefined) {
+      towerID.unshift(pieceinmotion);
+      pieceinmotion = undefined;
+      $(this).children().remove();
+      audio.play();
+      moves = moves + 1;
+  } else if (pieceinmotion < towerID[0]) {
+      towerID.unshift(pieceinmotion);
+      pieceinmotion = undefined;
+      $(this).children().remove();
+      audio.play();
+      moves = moves + 1;
+  } else {
+      $(this).children().remove();
+  };
+drawTowerFunction(towerID);
+};
+drawTowerFunction = function(towerID){
+if(towerID = tower1){
+  drawTower1();
+}
+else if (towerID = tower2){
+  drawTower2();
+}
+else{
+  drawTower3();
+}
+}
+*/
 interactwithtower1 = function() {
     if (pieceinmotion === undefined) {
         pieceinmotion = tower1.shift();
@@ -30,11 +90,13 @@ interactwithtower1 = function() {
         pieceinmotion = undefined;
         $(this).children().remove();
         audio.play();
+        moves = moves + 1;
     } else if (pieceinmotion < tower1[0]) {
         tower1.unshift(pieceinmotion);
         pieceinmotion = undefined;
         $(this).children().remove();
         audio.play();
+        moves = moves + 1;
     } else {
         $(this).children().remove();
     }
@@ -50,11 +112,13 @@ interactwithtower2 = function() {
         pieceinmotion = undefined;
         $(this).children().remove();
         audio.play();
+        moves = moves + 1;
     } else if (pieceinmotion < tower2[0]) {
         tower2.unshift(pieceinmotion);
         pieceinmotion = undefined;
         $(this).children().remove();
         audio.play();
+        moves = moves + 1;
     } else {
         $(this).children().remove();
     }
@@ -71,18 +135,24 @@ interactwithtower3 = function() {
         pieceinmotion = undefined;
         $(this).children().remove();
         audio.play();
+        moves = moves + 1;
     } else if (pieceinmotion < tower3[0]) {
         tower3.unshift(pieceinmotion);
         pieceinmotion = undefined;
         $(this).children().remove();
         audio.play();
+        moves = moves + 1;
     } else {
         $(this).children().remove();
     }
     checkWinCondition();
     drawTower3();
 };
-
+/*
+$('.tower1').on("click", interactwithtower(tower1));
+$('.tower2').on("click", interactwithtower(tower2));
+$('.tower3').on("click", interactwithtower(tower3));
+*/
 $('.tower1').on("click", interactwithtower1);
 $('.tower2').on("click", interactwithtower2);
 $('.tower3').on("click", interactwithtower3);
@@ -100,6 +170,7 @@ changeDifficulty = function() {
     tower2 = [];
     tower3 = [];
     winningtower=[];
+    moves = 0;
     $('#alltowers').children().children().remove();
     for (i = 0; i < this.id; i++) {
         tower1.push(i + 1);
